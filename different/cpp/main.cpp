a6d136e291e2cb3759408124d48bcfd2bbf15755
csm-yujinkim <60201675+csm-yujinkim@users.noreply.github.com>
2020-06-04 19:43:54 -0700

    Working c++ solution for different (#24)

---
+++ different/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    long long a, b;
+    while (std::cin >> a >> b) {
+        std::cout << std::abs(a - b) << std::endl;
+    }
+    return 0;
+}
