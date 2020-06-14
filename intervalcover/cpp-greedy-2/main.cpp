aa99b557481f9322a4a8e923d8dbe6043c74f83c
YuJin Kim <yujinkim@mymail.mines.edu>
2020-06-14 16:27:08 -0700

    Reorganize

--- intervalcover/cpp-greedy-2/main.cpp
+++ intervalcover/cpp-greedy-2/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <list>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+struct interval {
+    double start, end;
+
+    interval(double start, double end) : start(start), end(end) {}
+
+    [[nodiscard]] bool proper() const {
+        return start < end;
+    }
+
+    [[nodiscard]] bool covers(interval other) const {
+        return this->start <= other.start && other.end <= this->end;
+    }
+};
+
+namespace help {
+    static std::pair<ssize_t, interval>
+    max_cover(std::list<ssize_t> const &indices, std::vector<interval> const &intervals, interval range) {
+        double max_coverage = -1.0;
+        ssize_t max_index = -1L, index = 0L;
+        for (interval i : intervals) {
+            double const coverage = std::max(0.0, std::min(i.end, range.end) - std::min(i.start, range.start));
+            if (max_coverage < coverage) {
+                max_coverage = coverage;
+                max_index = index;
+            }
+            ++index;
+        }
+        return std::make_pair(max_index, intervals[max_index]);
+    }
 }
