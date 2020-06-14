f75e51c2113429a31a81f8b5feb5961ae016b039
YuJin Kim <yujinkim@mymail.mines.edu>
2020-06-14 16:43:37 -0700

    Add needed helper functions

--- intervalcover/cpp-greedy-2/main.cpp
+++ intervalcover/cpp-greedy-2/main.cpp
@@ -32,4 +32,39 @@ namespace help {
         }
         return std::make_pair(max_index, intervals[max_index]);
     }
+
+    static bool strictly_disjoint(interval a, interval b) {
+        return a.end < b.start || b.end < a.start;
+    }
+
+    static bool
+    union_covers(std::list<ssize_t> const &indices, std::vector<interval> const &intervals, interval range) {
+        double leftmost_endpoint, rightmost_endpoint;
+        {
+            for (ssize_t const i : indices) {
+                interval const n = intervals[i];
+                leftmost_endpoint = std::min(leftmost_endpoint, n.start);
+                rightmost_endpoint = std::max(rightmost_endpoint, n.end);
+            }
+        }
+        return leftmost_endpoint <= range.start && range.end <= rightmost_endpoint;
+    }
+
+    static std::vector<interval> forward_difference(interval minuend, interval subtrahend) {
+        // The subtrahend is subtracted FROM the minuend.
+        // The end result may be a list (vector) of 0, 1 or 2 intervals.
+        // If two intervals result, then the first one comes before (has lower coordinates) than the other.
+        // Both the minuend and the subtrahend are assumed to be proper. (This is not checked.)
+        // The resultants will also be proper.
+        interval const left(minuend.start, subtrahend.start);
+        interval const right(subtrahend.end, minuend.end);
+        std::vector<interval> resultants;
+        if (left.proper()) {
+            resultants.push_back(left);
+        }
+        if (right.proper()) {
+            resultants.push_back(right);
+        }
+        return resultants;
+    }
 }
