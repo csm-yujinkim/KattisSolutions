cc1dfa39bc3e8278d7f279fcd6123b5d0cf9d67a
YuJin Kim <yujinkim@mymail.mines.edu>
2020-06-16 22:18:16 -0700

    Draft an implementation for memoized catalan algorithm
    Using the recurrence formula
    
    C(0) = 1
    C(n + 1) = SUM[i = 0, i = n, C(i) * C(n - i)]

--- catalan/cpp/main.cpp
+++ catalan/cpp/main.cpp
@@ -1,4 +1,38 @@
 #include <iostream>
+#include <unordered_map>
+
+typedef unsigned long long i128;
+
+namespace impl {
+    static std::unordered_map<i128, i128> catalan_memo {
+            {0ULL, 1ULL}
+    };
+
+    static i128 catalan(i128 x);
+
+    static i128 make_catalan(i128 x);
+
+    static i128 catalan(i128 const x) {
+        if (catalan_memo.count(x)) {
+            return catalan_memo.at(x);
+        } else {
+            i128 const y = make_catalan(x);
+            catalan_memo[x] = y;
+            return y;
+        }
+    }
+
+    static i128 make_catalan(i128 const x) {
+        i128 const xm1 = x - 1ULL;
+        i128 sum = 0ULL;
+        for (i128 i = 0ULL; i < xm1; ++i) {
+            sum += catalan(i) + catalan(xm1 - i);
+        }
+        return sum;
+    }
+}
+
+static i128 catalan(i128 x) { return impl::catalan(x); }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
