3f9897503ba71b3585d51143bf7d2fa69fc8b9ed
YuJin Kim <yujinkim@mymail.mines.edu>
2020-06-16 22:26:10 -0700

    Rename i128 -> u128 to reflect its unsigned nature

--- catalan/cpp/main.cpp
+++ catalan/cpp/main.cpp
@@ -2,54 +2,54 @@
 #include <unordered_map>
 #include <vector>
 
-typedef unsigned long long i128;
+typedef unsigned long long u128;
 
 namespace impl {
-    static std::unordered_map<i128, i128> catalan_memo {
+    static std::unordered_map<u128, u128> catalan_memo {
             {0ULL, 1ULL}
     };
 
-    static i128 catalan(i128 x);
+    static u128 catalan(u128 x);
 
-    static i128 make_catalan(i128 x);
+    static u128 make_catalan(u128 x);
 
-    static i128 catalan(i128 const x) {
+    static u128 catalan(u128 const x) {
         if (catalan_memo.count(x)) {
             return catalan_memo.at(x);
         } else {
-            i128 const y = make_catalan(x);
+            u128 const y = make_catalan(x);
             catalan_memo[x] = y;
             return y;
         }
     }
 
-    static i128 make_catalan(i128 const x) {
-        i128 const xm1 = x - 1ULL;
-        i128 prod = 0ULL;
-        for (i128 i = 0ULL; i <= xm1; ++i) {
+    static u128 make_catalan(u128 const x) {
+        u128 const xm1 = x - 1ULL;
+        u128 prod = 0ULL;
+        for (u128 i = 0ULL; i <= xm1; ++i) {
             prod += catalan(i) * catalan(xm1 - i);
         }
         return prod;
     }
 }
 
-static i128 catalan(i128 x) { return impl::catalan(x); }
+static u128 catalan(u128 x) { return impl::catalan(x); }
 
 int main() {
     size_t q;
     std::cin >> q;
-    std::vector<i128> xs, ys;
+    std::vector<u128> xs, ys;
     xs.reserve(q);
     ys.reserve(q);
     while (q--) {
-        i128 x;
+        u128 x;
         std::cin >> x;
         xs.push_back(x);
     }
-    for (i128 const x : xs) {
+    for (u128 const x : xs) {
         ys.push_back(catalan(x));
     }
-    for (i128 const y : ys) {
+    for (u128 const y : ys) {
         std::cout << y << '\n';
     }
     return 0;
